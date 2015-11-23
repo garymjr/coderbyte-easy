5e818b7a81ca48ff86c1217a801d9556d4863978
garymjr <garymjr@gmail.com>
2015-11-22 23:38:35 -0500

    longest word

---
+++ longest-word.js
@@ -0,0 +1,14 @@
+function LongestWord(sen) {
+	var longest = ''
+
+	sen = sen.replace(/[^\w\s]/g, '').split(' ')
+	sen.forEach(function(word) {
+		if (word.length > longest.length) {
+			longest = word
+		}
+	})
+	return longest
+}
+
+console.log(LongestWord('fun&!! time'))
+console.log(LongestWord('I love dogs'))
