886feef7c23098d05cf4d5e7d59431006c6c9888
garymjr <garymjr@gmail.com>
2015-11-23 10:00:04 -0500

    arith geo

---
+++ arith-geo.js
@@ -0,0 +1,29 @@
+function ArithGeo(arr) {
+  var isArith, isGeo
+  var arith = arr[1] - arr[0]
+  var geo = arr[1] / arr[0]
+
+  for (var i = 0; i < arr.length - 1; i++) {
+    if (arr[i] + arith === arr[i+1]) {
+      isArith = true
+      isGeo = false
+    } else if (arr[i] * geo === arr[i+1]) {
+      isArith = false
+      isGeo = true
+    } else {
+      isArith = false
+      isGeo = false
+    }
+  }
+
+  if (isArith) {
+    return 'Arithmetic'
+  } else if (isGeo) {
+    return 'Geometric'
+  } else {
+    return -1
+  }
+}
+
+console.log(ArithGeo([5, 10, 15]))
+console.log(ArithGeo([2, 4, 16, 24]))
