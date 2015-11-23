11ff3087a96f4105ccfbdacca33a0e48d68ecb66
garymjr <garymjr@gmail.com>
2015-11-23 09:10:25 -0500

    vowel count

---
+++ vowel-count.js
@@ -0,0 +1,17 @@
+function VowelCount(str) {
+	var vowels = ['a', 'e', 'i', 'o', 'u']
+	var count = 0
+	str = str.toLowerCase().split('')
+
+	for (var i = 0; i < str.length; i++) {
+		vowels.forEach(function(vowel) {
+			if (str[i] === vowel) {
+				count++
+			}
+		})
+	}
+	return count
+}
+
+console.log(VowelCount('hello'))
+console.log(VowelCount('coderbyte'))
