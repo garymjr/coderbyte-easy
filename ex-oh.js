29d60ea67288459504f2be9c5edc4cad7ef2377e
garymjr <garymjr@gmail.com>
2015-11-23 09:19:17 -0500

    ex oh

---
+++ ex-oh.js
@@ -0,0 +1,16 @@
+function ExOh(str) {
+	var x = 0, o = 0
+	str = str.split('')
+
+	str.forEach(function(char) {
+		if (char === 'x') {
+			x++
+		} else {
+			o++
+		}
+	})
+	return x === o
+}
+
+console.log(ExOh('xooxxo'))
+console.log(ExOh('x'))
