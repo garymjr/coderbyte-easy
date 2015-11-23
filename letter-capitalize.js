38e23abaecddcf83a830f24f4d078af665a2fe04
garymjr <garymjr@gmail.com>
2015-11-23 00:27:53 -0500

    letter capitalize

---
+++ letter-capitalize.js
@@ -0,0 +1,12 @@
+function LetterCapitalize(str) {
+	str = str.split(' ')
+
+	for (var i = 0; i < str.length; i++) {
+		str[i] = str[i].charAt(0).toUpperCase() + str[i].substr(1)
+	}
+
+	return str.join(' ')
+}
+
+console.log(LetterCapitalize('hello world'))
+console.log(LetterCapitalize('i ran there'))
