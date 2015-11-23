c7604484692b14bca0050071be1a067209463676
garymjr <garymjr@gmail.com>
2015-11-23 09:33:46 -0500

    palindrome

---
+++ palindrome.js
@@ -0,0 +1,8 @@
+function Palindrome(str) {
+  str = str.replace(/\s/g, '')
+  var palindrome = str
+  return str === palindrome.split('').reverse().join('')
+}
+
+console.log(Palindrome('never odd or even'))
+console.log(Palindrome('eye'))
