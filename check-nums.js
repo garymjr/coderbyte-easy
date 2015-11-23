e18a3cec2cda35af8f930e9c5bd6b06e6f73af05
garymjr <garymjr@gmail.com>
2015-11-23 01:13:31 -0500

    check nums

---
+++ check-nums.js
@@ -0,0 +1,12 @@
+function CheckNums(num1, num2) {
+	if (num2 > num1) {
+		return true
+	} else if (num2 < num1) {
+		return false
+	}
+
+	return -1
+}
+
+console.log(CheckNums(3, 122))
+console.log(CheckNums(67, 67))
