e6b4a86cea91bd1158d20eeeb470b30c51f25e14
garymjr <garymjr@gmail.com>
2015-11-23 15:56:12 -0500

    division stringfield

---
+++ division-stringfield.js
@@ -0,0 +1,8 @@
+function DivisionStringfield(num1, num2) {
+  var total = Math.round(num1 / num2)
+
+  return total.toLocaleString()
+}
+
+console.log(DivisionStringfield(5, 2))
+console.log(DivisionStringfield(6874, 67))
