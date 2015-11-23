7303c635e9c5f0d8806ba911f46c72bc0d68e9c8
garymjr <garymjr@gmail.com>
2015-11-23 00:12:52 -0500

    letter changes

---
+++ letter-changes.js
@@ -0,0 +1,20 @@
+function LetterChanges(str) {
+	var vowels = ['a', 'e', 'i', 'o', 'u']
+	var newStr = []
+	str = str.split('')
+
+	str.forEach(function(letter) {
+		if (letter >= 'a' && letter <= 'z') {
+			newStr.push(String.fromCharCode(letter.charCodeAt() + 1))
+		} else {
+			newStr.push(letter)
+		}
+	})
+
+	return newStr.join('').replace(/[aeiou]/g, function (letter) {
+		return letter.toUpperCase()
+	})
+}
+
+console.log(LetterChanges('hello*3'))
+console.log(LetterChanges('fun times!'))
