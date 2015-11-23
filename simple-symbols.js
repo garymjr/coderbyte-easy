532b232838eea1eedafa412320cd3253a09455bf
garymjr <garymjr@gmail.com>
2015-11-23 01:07:43 -0500

    simple symbols

---
+++ simple-symbols.js
@@ -0,0 +1,18 @@
+function SimpleSymbols (str) {
+	var re = /[A-za-z]/
+	str = str.split('')
+
+
+	for (var i = 0; i < str.length; i++) {
+		if (re.test(str[i])) {
+			if (str[i - 1] != '+' || str[i + 1] != '+') {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+console.log(SimpleSymbols('+d+=3=+s+'))
+console.log(SimpleSymbols('f++d+'))
