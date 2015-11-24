258472b636817661310026a79feb824d1a56ea6e
garymjr <garymjr@gmail.com>
2015-11-23 23:15:29 -0500

    mean mode

---
+++ mean-mode.js
@@ -0,0 +1,36 @@
+function MeanMode(arr) {
+  var mean = 0
+  var modeCount = []
+  var mode = 0
+  arr = arr.sort(function(a, b) { return a - b })
+
+  for (var i = 0; i < arr.length; i++) {
+    mean += arr[i]
+  }
+
+  mean = mean / arr.length
+
+  for (var i = 0; i < arr.length; i++) {
+    modeCount[i] = 1
+    for (var j = 0; j < arr.length; j++) {
+      if (i != j & arr[i] === arr[j]) {
+        modeCount[i]++
+      }
+    }
+  }
+
+  for (var i = 0; i < arr.length; i++) {
+    if (modeCount[i] > modeCount[mode]) {
+      mode = i
+    }
+  }
+
+  if (arr[mode] === mean) {
+    return 1
+  } else {
+    return 0
+  }
+}
+
+console.log(MeanMode([1, 2, 3]))
+console.log(MeanMode([4, 4, 4, 6, 2]))
