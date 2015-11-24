b808bbc605d4b9d2bb8a673eae6332a6bb8ea4a6
garymjr <garymjr@gmail.com>
2015-11-24 18:11:43 -0500

    dash insert

---
+++ dash-insert.js
@@ -0,0 +1,20 @@
+function DashInsert(str) {
+  var offset = 0
+    , dashes = []
+  str = str.split('')
+
+  for (var i = 0; i < str.length; i++) {
+    if (str[i] % 2 != 0 && str[i+1] % 2 != 0) {
+      dashes[i+offset] = str[i]
+      dashes[i+offset+1] = '-'
+      offset++
+    } else {
+        dashes[i+offset] = str[i]
+      }
+  }
+
+  return dashes.join('')
+}
+
+console.log(DashInsert('99946'))
+console.log(DashInsert('56730'))
