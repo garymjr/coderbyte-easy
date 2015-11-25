6a099d6042ee41b2c7d0b77547cb427c1e2dd47d
garymjr <garymjr@gmail.com>
2015-11-24 20:27:23 -0500

    swap case

---
+++ swap-case.js
@@ -0,0 +1,16 @@
+function SwapCase(str) {
+  str = str.split('')
+
+  for (var i = 0; i < str.length; i++) {
+    if (str[i] === str[i].toUpperCase()) {
+      str[i] = str[i].toLowerCase()
+    } else {
+      str[i] = str[i].toUpperCase()
+    }
+  }
+
+  return str.join('')
+}
+
+console.log(SwapCase('Hello-LOL'))
+console.log(SwapCase('Sup DUDE!!?'))
