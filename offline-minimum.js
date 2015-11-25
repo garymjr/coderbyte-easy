5132f61a513d1fc227d130cdb937da8351a15936
garymjr <garymjr@gmail.com>
2015-11-25 13:00:52 -0500

    offline minumim

---
+++ offline-minimum.js
@@ -0,0 +1,17 @@
+function OfflineMinimum (strArr) {
+  var count = 0
+    , nums = []
+
+  for (var i = 0; i < strArr.length; i++) {
+    if (strArr[i] != 'E') {
+      nums.push(strArr[i])
+    } else {
+      count++
+    }
+  }
+
+ return nums.slice(0, count).join(',')
+}
+
+console.log(OfflineMinimum(['1', '2', 'E', 'E', '3']))
+console.log(OfflineMinimum(['4', 'E', '1', 'E', '2', 'E', '3', 'E']))
