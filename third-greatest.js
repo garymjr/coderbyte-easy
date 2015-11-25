4cb6d0712b694a8aecc1280293a1a27a002fe2d0
garymjr <garymjr@gmail.com>
2015-11-24 21:14:19 -0500

    third greatest

---
+++ third-greatest.js
@@ -0,0 +1,10 @@
+function ThirdGreatest (strArr) {
+  strArr = strArr.sort(function (a, b) {
+    return b.length - a.length
+  })
+
+  return strArr[2]
+}
+
+console.log(ThirdGreatest(['coder', 'byte', 'code']))
+console.log(ThirdGreatest(['abc', 'defg', 'z', 'hijk']))
